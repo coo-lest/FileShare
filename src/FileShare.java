d4493df90b0b2f698f28a94b082028c744319faf
coo-lest <chen.dz@live.com>
2020-11-08 19:42:22 +0800

    Client login WIP: send username and password

--- src/FileShare.java
+++ src/FileShare.java
@@ -1,21 +1,52 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class FileShare {
     final int SVR_TCP = 9999;
     FileShareServer fileShareServer;
     FileShareClient fileShareClient;
+    static Scanner scanner;
 
     public FileShare() throws IOException {
         fileShareServer = new FileShareServer(SVR_TCP);
         fileShareClient = new FileShareClient();
+        scanner = new Scanner(System.in);
     }
 
-    void cli() {
-        // cli goes here
+    void cli() throws IOException {
+        while (true) {
+            // Read command
+            System.out.print("> ");
+            String[] cmd = scanner.nextLine().trim().split("\\s+");
+
+            switch (cmd[0]) {
+                case "login":
+                    if (cmd.length != 2) {
+                        System.out.println("Invalid number of arguments");
+                    }
+
+                    String[] loginInfo = cmd[1].split("@");  // cmd[1] format username@address
+                    if (loginInfo.length != 2) {
+                        System.out.println("Invalid argument \"" + cmd[1] + "\"");
+                        System.out.println("Usage: login username@address");
+                    }
+                    login(loginInfo[0], loginInfo[1]);
+                    break;
+
+
+                default:
+                    System.out.println("Unknown command");
+                    break;
+            }
+        }
+    }
+
+    private void login(String username, String address) throws IOException {
+        fileShareClient.login(username, address);
     }
 
     public static void main(String[] args) throws Exception{
         FileShare fileShare = new FileShare();
-
+        fileShare.cli();
     }
 }
--- src/FileShareClient.java
+++ src/FileShareClient.java
@@ -1,28 +1,68 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
 
 public class FileShareClient {
     Socket socket;
-    String svrIp = "localhost";
+    String svrIp;
     int svrPort = 9999;
+    DataInputStream din;
+    DataOutputStream dout;
 
     public FileShareClient() throws IOException {
         System.out.println("Client created");
-        socket = new Socket(svrIp, svrPort);
-        System.out.println("Connection established");
-        DataInputStream din = new DataInputStream(socket.getInputStream());
-        while (true) {
-            byte[] buffer = new byte[1024];
-            din.read(buffer);
-            System.out.println(new String(buffer));
-        }
+
+//        Thread dinThread = new Thread(() -> {
+//            while (true) {
+//                byte[] buffer = new byte[1024];
+//                try {
+//                    din.read(buffer);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                System.out.print(new String(buffer));
+//            }
+//        });
+//        dinThread.start();
 
     }
 
-    void login(String username, String password) {
+    void login(String username, String address) throws IOException {
+        svrIp = address;
+        socket = new Socket(svrIp, svrPort);
+        din = new DataInputStream(socket.getInputStream());
+        dout = new DataOutputStream(socket.getOutputStream());
 
+        sendMsg(dout, username);
+
+        System.out.println("message sent");
+        // Wait for request for password
+        System.out.print(receiveMsg(din));
+
+        // Get and send password
+        String password = FileShare.scanner.nextLine();
+        sendMsg(dout, password);
+
+
+    }
+
+    private String receiveMsg(DataInputStream din) throws IOException {
+        String msg = "";
+
+        int msgLen = din.readInt();
+        byte[] buffer = new byte[1024];
+        while (msgLen > 0) {
+            int read = din.read(buffer, 0, msgLen);
+            msg += new String(buffer);
+            msgLen -= read;
+        }
+
+        return msg;
+    }
+
+    private void sendMsg(DataOutputStream dout, String msg) throws IOException {
+        dout.writeInt(msg.length());
+        dout.writeBytes(msg);
     }
 }
